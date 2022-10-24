895bb7d3c77512b6a2ab75206af86925f386fbb0
Shahrukh <xazuskhan@gmail.com>
2022-10-24 22:07:35 +0500

    addd more functions in scripts

--- scripts/TokenInteraction.js
+++ scripts/TokenInteraction.js
@@ -71,10 +71,59 @@ async function addTeamAddress() {
   // console.log(web3.eth.accounts.wallet);
 }
 
+// This function will run when we need to run or change the address of the tax collector
+async function addContractAddress() {
+  web3.eth.accounts.wallet.add(PRIVATE_KEY);
+  const account = web3.eth.accounts.wallet[0].address;
+  const addContractAddress = await PhoneBotToken.methods.addContractAddress('0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba')
+    .send({ from: account,
+            gas: 300000, }, function (err, res) {
+      if (err) {
+        console.log("An error occured", err);
+        return;
+      }
+      console.log("Hash of the transaction: " + res);
+    });
+
+  web3.eth.accounts.wallet.remove(account);
+  // console.log(web3.eth.accounts.wallet);
+}
+
+async function getContractAddresses() {
+  web3.eth.accounts.wallet.add(PRIVATE_KEY);
+  const account = web3.eth.accounts.wallet[0].address;
+  const getContractAddresses = await PhoneBotToken.methods
+    .contractAccess('0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba')
+    .call({ from: account, gas: 300000, }, function (err, res) {
+      if (err) {
+        console.log("An error occured", err);
+        return;
+      }
+      console.log("Hash of the transaction: " + res);
+    });
+}
+
+async function mint() {
+  web3.eth.accounts.wallet.add(PRIVATE_KEY);
+  const account = web3.eth.accounts.wallet[0].address;
+  const mint = await PhoneBotToken.methods
+    .mint('0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba', 500)
+    .send({ from: account, gas: 300000, }, function (err, res) {
+      if (err) {
+        console.log("An error occured", err);
+        return;
+      }
+      console.log("Hash of the transaction: " + res);
+    });
+}
+
 async function main() {
   console.log(await getTokenValue());
   console.log(await getOwner());
   console.log(await addTeamAddress());
+  // console.log(await addContractAddress()); // Already Addeed: tx hash = 0xed17cd99be665261d67556b173a40c2a78241ee61dd6439f54330b30c4d53a02
+  console.log(await getContractAddresses());
+  // console.log(await mint()); // Already Called for mint : tx hash = 0x97cc0652b63614abf041390126ffad3c0b5edf369716898b7e1108f20a3f0610
 }
 
 main()
--- scripts/deployment/deployController.js
+++ scripts/deployment/deployController.js
@@ -7,16 +7,26 @@
 const hre = require("hardhat");
 
 async function main() {
-try {
-  const Controller = await hre.ethers.getContractFactory("Controller");
-  const controller = await Controller.deploy('0x5575bffAfeeC5F217A2F851bf3088110C60c83e2');
-  await controller.deployed();
+// try {
+//   const Controller = await hre.ethers.getContractFactory("Controller");
+//   const controller = await Controller.deploy('0x5575bffAfeeC5F217A2F851bf3088110C60c83e2');
+//   await controller.deployed();
 
-  console.log("Deployed to: ", controller.address);
-  process.exit(0);
-} catch (error) {
-  console.log(error);
-}
+
+
+//  DEPLOYED TO = 0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba;
+
+
+
+
+
+
+
+//   console.log("Deployed to: ", controller.address);
+//   process.exit(0);
+// } catch (error) {
+//   console.log(error);
+// }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
