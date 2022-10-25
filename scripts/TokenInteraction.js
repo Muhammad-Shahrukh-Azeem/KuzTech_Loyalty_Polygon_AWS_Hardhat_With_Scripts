cd936df7406cd7fcc6841334fa18f867693b0dce
Shahrukh <xazuskhan@gmail.com>
2022-10-25 17:56:42 +0500

    Making it dynamic

--- scripts/TokenInteraction.js
+++ scripts/TokenInteraction.js
@@ -21,53 +21,56 @@ console.log("signer done");
 
 const PhoneBotToken = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
 
+// THIS FUNCTION RETURNS THE BUYING PRICE OF TOKEN IN WEI
 async function getTokenValue() {
   const tokenValue = await PhoneBotToken.methods.tokenPrice().call();
   return tokenValue;
 }
 
+// THIS FUNCTION RETURNS THE ADDRESS OF THE OWNER
 async function getOwner() {
   const owner = await PhoneBotToken.methods.owner().call();
   return owner;
 }
 
-async function addTeamAddress() {
-  web3.eth.accounts.wallet.add(PRIVATE_KEY);
+// 
+async function addTeamAddress(userAddress) {
+  web3.eth.accounts.wallet.add(userAddress);
   const account = web3.eth.accounts.wallet[0].address;
-  // console.log(web3.eth.accounts.wallet);
+  console.log(web3.eth.accounts.wallet);
 
-  // const addTeamAddress = await PhoneBotToken.methods
-  //   .addTeamAddress("0x8BDB54Ac76ca5d98E8328E0AC13e07a693a97e30")
-  //   .send({ from: account,
-  //           gas: 300000, }, function (err, res) {
-  //     if (err) {
-  //       console.log("An error occured", err);
-  //       return;
-  //     }
-  //     console.log("Hash of the transaction: " + res);
-  //   });
-
-  await PhoneBotToken.methods
-    .teamAccessRecord("0x8BDB54Ac76ca5d98E8328E0AC13e07a693a97e30")
-    .call({}, function (err, res) {
+  const addTeamAddress = await PhoneBotToken.methods
+    .addTeamAddress("0x8BDB54Ac76ca5d98E8328E0AC13e07a693a97e30")
+    .send({ from: account, gas: 300000 }, function (err, res) {
       if (err) {
         console.log("An error occured", err);
         return;
       }
       console.log("Hash of the transaction: " + res);
     });
+}
 
-  web3.eth.accounts.wallet.remove(account);
-  // console.log(web3.eth.accounts.wallet);
+async function checkTeamAddress() {
+  return await PhoneBotToken.methods
+    .teamAccessRecord("0x8BDB54Ac76ca5d98E8328E0AC13e07a693a97e30")
+    .call();
+  //   if (err) {
+  //     console.log("An error occured", err);
+  //     return;
+  //   }
+  //   console.log("Hash of the transaction: " + res);
+  // });
+
+  // web3.eth.accounts.wallet.remove(account);
 }
 
 // This function will run when we need to run or change the address of the tax collector
-async function addContractAddress() {
-  web3.eth.accounts.wallet.add(PRIVATE_KEY);
+async function addContractAddress(userAddress, newContractAddress) {
+  web3.eth.accounts.wallet.add(userAddress);
   const account = web3.eth.accounts.wallet[0].address;
-  const addContractAddress = await PhoneBotToken.methods.addContractAddress('0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba')
-    .send({ from: account,
-            gas: 300000, }, function (err, res) {
+  const addContractAddress = await PhoneBotToken.methods
+    .addContractAddress(newContractAddress)
+    .send({ from: account, gas: 300000 }, function (err, res) {
       if (err) {
         console.log("An error occured", err);
         return;
@@ -79,26 +82,18 @@ async function addContractAddress() {
   // console.log(web3.eth.accounts.wallet);
 }
 
-async function getContractAddresses(addresss) {
-  web3.eth.accounts.wallet.add(PRIVATE_KEY);
-  const account = web3.eth.accounts.wallet[0].address;
-  const getContractAddresses = await PhoneBotToken.methods
-    .contractAccess(addresss)
-    .call({ from: account, gas: 300000, }, function (err, res) {
-      if (err) {
-        console.log("An error occured", err);
-        return;
-      }
-      console.log("Hash of the transaction: " + res);
-    });
+async function getContractAddresses(contracrAddress) {
+  // web3.eth.accounts.wallet.add(PRIVATE_KEY);
+  // const account = web3.eth.accounts.wallet[0].address;
+  return await PhoneBotToken.methods.contractAccess(contracrAddress).call();
 }
 
-async function mint() {
-  web3.eth.accounts.wallet.add(PRIVATE_KEY);
+async function mint(userAddress) {
+  web3.eth.accounts.wallet.add(userAddress);
   const account = web3.eth.accounts.wallet[0].address;
   const mint = await PhoneBotToken.methods
-    .mint('0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba', 500)
-    .send({ from: account, gas: 300000, }, function (err, res) {
+    .mint("0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba", 500)
+    .send({ from: account, gas: 300000 }, function (err, res) {
       if (err) {
         console.log("An error occured", err);
         return;
@@ -110,8 +105,8 @@ async function mint() {
 async function main() {
   console.log(await getTokenValue());
   console.log(await getOwner());
-  console.log(await addTeamAddress());
-  // console.log(await addContractAddress()); // Already Addeed: tx hash = 0xed17cd99be665261d67556b173a40c2a78241ee61dd6439f54330b30c4d53a02
+  console.log(await checkTeamAddress(signer.address));
+  // console.log(await addContractAddress( , '0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba')); // Already Addeed: tx hash = 0xed17cd99be665261d67556b173a40c2a78241ee61dd6439f54330b30c4d53a02
   console.log(await getContractAddresses('0x8424b1E588e771Af5940EDAa35ADE2075A37E0Ba'));
   // console.log(await mint()); // Already Called for mint : tx hash = 0x97cc0652b63614abf041390126ffad3c0b5edf369716898b7e1108f20a3f0610
 }
